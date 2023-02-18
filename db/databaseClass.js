3b2ea517746c4dd02a76e0534540e8212944bc50
Ben Ropa <benropa@outlook.com>
2023-02-18 16:16:01 -0600

    setting up database class

--- db/databaseClass.js
+++ db/databaseClass.js
@@ -9,3 +9,11 @@ const writeFileAsync = util.promisify(fs.writeFile);
 const noteData = './db/db.json';
 
 // code for class to handle reading and writing data
+class Database {
+    async readNotes() {
+        try {
+            const rawNotes = await readFileAsync(noteData, 'UTF8');
+            return rawNotes ? JSON.parse(rawNotes) : [];
+        } catch (error) {
+            throw error;
+        }
