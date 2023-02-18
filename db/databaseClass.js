236d8887f67b1d3f178699b6c526cddfccbec00e
Ben Ropa <benropa@outlook.com>
2023-02-18 16:15:18 -0600

    adding code for database class

---
+++ db/databaseClass.js
@@ -0,0 +1,11 @@
+// set requirements
+const fs = require('fs');
+const util = require('util');
+
+//Asynchronous consts to read & write using util and fs
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const noteData = './db/db.json';
+
+// code for class to handle reading and writing data
