7b63671ea73df946cd78f02e31410cb12489408f
Ben Ropa <benropa@outlook.com>
2023-02-18 15:13:54 -0600

    adding server information

---
+++ server.js
@@ -0,0 +1,22 @@
+// Set dependencies
+const express = require('express')
+
+// initialize the application
+const app = express()
+const PORT = process.env.PORT || 3001
+
+// Data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Api routes
+const apiRoutes = require("./routes/apiRoutes");
+app.use(apiRoutes);
+const htmlRoutes = require("./routes/htmlRoutes");
+app.use(htmlRoutes);
+
+// set up application listener
+app.listen(PORT, () => {
+  console.log(`Listening on PORT: ${PORT}`);
+});
